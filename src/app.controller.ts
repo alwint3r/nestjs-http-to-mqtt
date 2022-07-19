0ec080d41af9fa843ba535a5991077b014d7bff1
Alwin Arrasyid <alwin.ridd@gmail.com>
2022-07-19 17:13:49 +0700

    remove await from emit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -12,6 +12,6 @@ export class AppController {
 
   @Post('/message')
   async postMessage(@Body() body: any) {
-    await this.mqttSink.emit('devices/message', body);
+    this.mqttSink.emit('devices/message', body);
   }
 }
